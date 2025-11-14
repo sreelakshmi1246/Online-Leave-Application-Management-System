5a874f4c067dbcaa0bfc4c87891ad0717fc1238b
Sreelakshmi1246 <sreelakshmichandran754@gmail.com>
2025-11-14 13:51:55 +0530

    Implemented backend middlewares

---
+++ server/src/middlewares/roleCheck.js
@@ -0,0 +1,9 @@
+const roleCheck = (...allowedRoles) => (req, res, next) => {
+  if (!req.user) return res.status(401).json({ message: 'Not authenticated' });
+  if (!allowedRoles.includes(req.user.role)) {
+    return res.status(403).json({ message: 'Forbidden' });
+  }
+  next();
+};
+
+export default roleCheck;
